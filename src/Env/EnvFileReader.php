4b5285aa39dfc827af43a8bfddc04ec7cc480944
Pierre Gaimard <pierre@gaimard-family.fr>
2020-10-03 13:09:20 +0200

    Remove file_exists test in EnvFileReader

--- src/Env/EnvFileReader.php
+++ src/Env/EnvFileReader.php
@@ -2,25 +2,15 @@
 
 namespace Framework3\Env;
 
-use RuntimeException;
-
 class EnvFileReader
 {
     /**
      * @param string $path
      *
-     * @return array|false
-     *
-     * @throws RuntimeException
+     * @return array
      */
     public function getFile(string $path): array
     {
-        if (!file_exists($path)) {
-            throw new RuntimeException(
-                sprintf('.env Manager exception: The env file "%s" do not exists', $path)
-            );
-        }
-
         return file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     }
 }
