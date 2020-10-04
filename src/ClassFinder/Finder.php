371b0aa6ea20bc5eb32b70d2fe92a153309cd7b5
Pierre Gaimard <pierre@gaimard-family.fr>
2020-10-04 11:37:50 +0200

    Remove getDirPathFromNamespace() and scan() methods from Finder class

--- src/ClassFinder/Finder.php
+++ src/ClassFinder/Finder.php
@@ -7,21 +7,20 @@
 namespace Framework3\ClassFinder;
 
 use Framework3\Exception\AppException;
-use Framework3\Filesystem\DirectoryReader;
 
 class Finder implements FinderInterface
 {
     /**
      * Used to retrieve PSR-4 autoload configuration.
      *
-     * @var AutoloadConfig
+     * @var AutoloadTools
      */
-    private AutoloadConfig $autoloadConfig;
+    private AutoloadTools $autoloadTools;
 
     /**
-     * @var DirectoryReader
+     * @var Scanner
      */
-    private DirectoryReader $dirReader;
+    private Scanner $scanner;
 
     /**
      * @var string
@@ -29,14 +28,14 @@ class Finder implements FinderInterface
     private string $baseDir;
 
     /**
-     * @param AutoloadConfig  $autoloadConfig
-     * @param DirectoryReader $dirReader
-     * @param string          $baseDir
+     * @param AutoloadTools $autoloadTools
+     * @param Scanner       $scanner
+     * @param string        $baseDir
      */
-    public function __construct(AutoloadConfig $autoloadConfig, DirectoryReader $dirReader, string $baseDir)
+    public function __construct(AutoloadTools $autoloadTools, Scanner $scanner, string $baseDir)
     {
-        $this->autoloadConfig = $autoloadConfig;
-        $this->dirReader = $dirReader;
+        $this->autoloadTools = $autoloadTools;
+        $this->scanner = $scanner;
         $this->baseDir = $baseDir;
     }
 
@@ -67,8 +66,8 @@ class Finder implements FinderInterface
      */
     public function getClassesList(string $namespace, $scanSubDir = true, string $suffix = null): ?array
     {
-        $relativeFilesPaths = $this->scan(
-            $this->baseDir . DIRECTORY_SEPARATOR . $this->getDirPathFromNamespace($namespace),
+        $relativeFilesPaths = $this->scanner->scan(
+            $this->baseDir . DIRECTORY_SEPARATOR . $this->autoloadTools->getDirPathFromNamespace($namespace),
             null,
             $scanSubDir
         );
@@ -97,77 +96,7 @@ class Finder implements FinderInterface
         return $classList;
     }
 
-    /**
-     * scans a directory and returns an array of absolute files path from a base directory.
-     *
-     * If $subDir is given, it will be added to $baseDir ($baseDir . '/' . $subDir)
-     * to set the directory who will be scan.
-     *
-     *      For `$baseDir = "/var/www/project"`
-     *      and `$subDir = "public"`
-     *      the final scan dir will be `/var/www/project/public`
-     *
-     * @param string      $baseDir      Absolute Base directory path
-     * @param string|null $subDir       Sub directory (will be added to $baseDir if given)
-     * @param bool        $scanSubDirs  If set to true, the method will retrieve the sub directories files.
-     *
-     * @return array|null
-     */
-    private function scan(string $baseDir, string $subDir = null, $scanSubDirs = true): ?array
-    {
-        $dir = ($subDir) ? $baseDir . DIRECTORY_SEPARATOR . $subDir : $baseDir;
-        $list = array_diff($this->dirReader->scan($dir), ['.', '..']);
-        $classList = [];
 
-        foreach ($list as $item) {
-            if ($this->dirReader->isDir($dir . DIRECTORY_SEPARATOR . $item)) {
-                if (!$scanSubDirs) {
-                    continue;
-                }
-
-                $childSubDir = ($subDir) ? $subDir . DIRECTORY_SEPARATOR . $item : $item;
-                $subClassList = $this->scan($baseDir, $childSubDir);
-
-                continue;
-            }
-
-            $classList[] = ($subDir) ?  $subDir . DIRECTORY_SEPARATOR . $item : $item;
-        }
-
-        if (!empty($subClassList)) {
-            $classList = array_merge($classList, $subClassList);
-        }
-
-        return (!empty($classList)) ? $classList : null;
-    }
-
-    /**
-     * converts a namespace to a relative file path (relative to the project base directory).
-     *
-     * @example namespace "App\Controller" will be converted to file path "src/Controller"
-     *
-     * @param $namespace
-     *
-     * @return string|string[]|null
-     *
-     * @throws AppException
-     */
-    private function getDirPathFromNamespace($namespace)
-    {
-        foreach ($this->autoloadConfig->getPsr4Config() as $baseNamespace => $basePath) {
-            $pattern = '#^' . str_replace('\\', '\\\\', $baseNamespace) . '#';
-
-            if (preg_match($pattern, $namespace)) {
-                return str_replace('\\', '/', preg_replace($pattern, $basePath, $namespace));
-            }
-        }
-
-        throw new AppException(
-            AppException::TYPE_CLASS_FINDER,
-            'Invalid namespace exception',
-            sprintf('The base of namespace "%s" do not exists in psr-4 composer.json autoload', $namespace)
-        );
-    }
 
     /**
      * Returns a fully qualified class namespace from a base namespace and a relative file path.
