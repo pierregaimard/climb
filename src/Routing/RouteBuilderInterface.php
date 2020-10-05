f88bd1edb2f7e674fc987df7e653efdb4d4db6d0
Pierre Gaimard <pierre@gaimard-family.fr>
2020-10-05 19:40:34 +0200

    Add Routing\RouteBuilderInterface interface

---
+++ src/Routing/RouteBuilderInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Framework3\Routing;
+
+interface RouteBuilderInterface
+{
+    /**
+     * Should Return a route path from route name and an array of variable data if present.
+     *
+     * This Method must return a string of route path,
+     * or throws an exception if no route declaration matches width the given name.
+     *
+     * @param string     $routeName Name given in route declaration.
+     * @param array|null $data      Array of data for the route. (if route path contains variable data)
+     *
+     * @return string
+     */
+    public function getRoutePath(string $routeName, array $data = null);
+}
