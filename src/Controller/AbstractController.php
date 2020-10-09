4ef672c198020a74b040d38c98ac72e7d9c8b2d5
Pierre Gaimard <pierre@gaimard-family.fr>
2020-10-09 08:28:49 +0200

    Add Controller\AbstractController class

---
+++ src/Controller/AbstractController.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace Framework3\Controller;
+
+use Framework3\Config\ConfigBag;
+use Framework3\Http\RedirectResponse;
+use Framework3\Http\Request;
+use Framework3\Routing\RouteBuilderInterface;
+use Framework3\Service\Container;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use Framework3\Bag\Bag;
+use Framework3\Exception\AppException;
+use Framework3\Security\UserInterface;
+
+class AbstractController
+{
+    /**
+     * @var Request
+     */
+    private Request $request;
+
+    /**
+     * @var Container
+     */
+    private Container $container;
+
+    /**
+     * @var Environment
+     */
+    private Environment $environment;
+
+    /**
+     * @var RouteBuilderInterface
+     */
+    private RouteBuilderInterface $routerBuilder;
+
+    /**
+     * @param Request $request
+     */
+    public function setRequest(Request $request): void
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * @param Container $container
+     */
+    public function setContainer(Container $container): void
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param Environment $environment
+     */
+    public function setEnvironment(Environment $environment): void
+    {
+        $this->environment = $environment;
+    }
+
+    /**
+     * @param RouteBuilderInterface $routeBuilder
+     */
+    public function setRouteBuilder(RouteBuilderInterface $routeBuilder): void
+    {
+        $this->routerBuilder = $routeBuilder;
+    }
+
+    /**
+     * $this->environment->render() shortcut for controller
+     *
+     * @param string        $path
+     * @param array|null    $data
+     *
+     * @return string
+     *
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    protected function render(string $path, array $data = null): string
+    {
+        return ($data !== null) ? $this->environment->render($path, $data) : $this->environment->render($path);
+    }
+
+    /**
+     * $this->router->setRoutePath() Shortcut for controller
+     *
+     * @param string $name
+     * @param array|null $data
+     *
+     * @return string
+     */
+    protected function getRoutePath(string $name, ?array $data = null): string
+    {
+        return $this->routerBuilder->getRoutePath($name, $data);
+    }
+
+    /**
+     * @param string $path
+     * @param array|null $routeData
+     * @param array|null $data
+     *
+     * @return RedirectResponse
+     */
+    protected function redirectToRoute(string $path, ?array $routeData = null, ?array $data = null): RedirectResponse
+    {
+        return new RedirectResponse($this->getRoutePath($path, $routeData), $data);
+    }
+
+    /**
+     * @param string $path
+     * @param bool $required
+     *
+     * @throws AppException
+     *
+     * @return ConfigBag|false
+     */
+    protected function getConfig(string $path, bool $required = false)
+    {
+        return $this->container->getConfig($path, $required);
+    }
+
+    /**
+     * @return Request
+     */
+    protected function getRequest(): Request
+    {
+        return $this->request;
+    }
+
+    /**
+     * Shortcut to retrieve current user from the session.
+     *
+     * @return UserInterface|null
+     */
+    protected function getUser()
+    {
+        return $this->request->getSession()->getUser();
+    }
+
+    /**
+     * Shortcut to check user access.
+     *
+     * @param string $role
+     *
+     * @return bool
+     */
+    protected function isGranted(string $role)
+    {
+        if ($this->request->getSession()->hasUser()) {
+            return $this->request->getSession()->getUser()->isGranted($role);
+        }
+
+        return false;
+    }
+
+    /**
+     * @return Bag
+     */
+    protected function getFlashes()
+    {
+        return $this->request->getSession()->getFlashes();
+    }
+
+    /**
+     * @return Bag
+     */
+    protected function getRequestData()
+    {
+        return $this->request->getSession()->getRequestData();
+    }
+}
