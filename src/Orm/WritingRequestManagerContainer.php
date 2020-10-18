cc7810687d5e734180585b45e477aa94b8d6ae2e
Pierre Gaimard <pierre@gaimard-family.fr>
2020-10-18 21:29:30 +0200

    Add Orm\WritingRequestManagerContainer class

---
+++ src/Orm/WritingRequestManagerContainer.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Framework3\Orm;
+
+use Framework3\Exception\AppException;
+
+class WritingRequestManagerContainer
+{
+    /**
+     * @var DbConnectionManager
+     */
+    private DbConnectionManager $connectionManager;
+
+    /**
+     * @var EntityMappingManager
+     */
+    private EntityMappingManager $mappingManager;
+
+    /**
+     * @var EntityDataManager
+     */
+    private EntityDataManager $dataManager;
+
+    /**
+     * @var InsertRequestBuilder
+     */
+    private InsertRequestBuilder $insertBuilder;
+
+    /**
+     * @var UpdateRequestBuilder
+     */
+    private UpdateRequestBuilder $updateBuilder;
+
+    /**
+     * @var DeleteRequestBuilder
+     */
+    private DeleteRequestBuilder $deleteBuilder;
+
+    public function __construct(
+        DbConnectionManager $connectionManager,
+        EntityMappingManager $mappingManager,
+        EntityDataManager $dataManager,
+        InsertRequestBuilder $insertBuilder,
+        UpdateRequestBuilder $updateBuilder,
+        DeleteRequestBuilder $deleteBuilder
+    ) {
+        $this->connectionManager = $connectionManager;
+        $this->mappingManager    = $mappingManager;
+        $this->dataManager       = $dataManager;
+        $this->insertBuilder     = $insertBuilder;
+        $this->updateBuilder     = $updateBuilder;
+        $this->deleteBuilder     = $deleteBuilder;
+    }
+
+    /**
+     * @param string $connection
+     *
+     * @return InsertRequestManager
+     *
+     * @throws AppException
+     */
+    public function getInsertManager(string $connection): InsertRequestManager
+    {
+        return new InsertRequestManager(
+            $this->connectionManager->getPdo($connection),
+            $this->mappingManager,
+            $this->dataManager,
+            $this->insertBuilder
+        );
+    }
+
+    /**
+     * @param string $connection
+     *
+     * @return UpdateRequestManager
+     *
+     * @throws AppException
+     */
+    public function getUpdateManager(string $connection): UpdateRequestManager
+    {
+        return new UpdateRequestManager(
+            $this->connectionManager->getPdo($connection),
+            $this->mappingManager,
+            $this->dataManager,
+            $this->updateBuilder
+        );
+    }
+
+    /**
+     * @param string $connection
+     *
+     * @return DeleteRequestManager
+     *
+     * @throws AppException
+     */
+    public function getDeleteManager(string $connection): DeleteRequestManager
+    {
+        return new DeleteRequestManager(
+            $this->connectionManager->getPdo($connection),
+            $this->mappingManager,
+            $this->dataManager,
+            $this->deleteBuilder
+        );
+    }
+}
