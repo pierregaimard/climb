16b69f13814fc17cf2aff5d12f334a15123fe3de
Pierre Gaimard <pierre@gaimard-family.fr>
2020-10-18 21:20:47 +0200

    Add Orm\DbConnectionManager class

---
+++ src/Orm/DbConnectionManager.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Framework3\Orm;
+
+use PDO;
+use Framework3\Exception\AppException;
+
+class DbConnectionManager
+{
+    /**
+     * @var DbConnector
+     */
+    private DbConnector $connector;
+
+    public function __construct(DbConnector $connector)
+    {
+        $this->connector = $connector;
+    }
+
+    /**
+     * @var PDO[]
+     */
+    private array $connections = [];
+
+    /**
+     * @param string $connection
+     *
+     * @return PDO
+     *
+     * @throws AppException
+     */
+    public function getPdo(string $connection): PDO
+    {
+        if (!$this->has($connection)) {
+            $this->set($connection);
+        }
+
+        return $this->connections[$connection];
+    }
+
+    /**
+     * @param string $connection
+     *
+     * @throws AppException
+     */
+    private function set(string $connection): void
+    {
+        $this->connections[$connection] = $this->connector->getPdo($connection);
+    }
+
+    /**
+     * @param string $connection
+     *
+     * @return bool
+     */
+    private function has(string $connection): bool
+    {
+        return array_key_exists($connection, $this->connections);
+    }
+}
